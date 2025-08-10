bc74d26666d4332e7fc52ee214c80c5ea5a651a0
myh0728 <brightmoonchiefyue@gmail.com>
2025-08-10 19:42:41 +0800

    [Feature] cumuSIR_R() and cumuSAVE_R()

---
+++ demo/DemoCompare_SemiDR.R
@@ -0,0 +1,12 @@
+n <- 100
+p <- 3
+
+X <- matrix(rnorm(n * p), nrow = n, ncol = p)
+beta0 <- c(1, rep(0.5, p - 1))
+Y <- as.vector(sin(X %*% beta0)) + rnorm(n, mean = 0, sd = 0.2)
+
+test <- cumuSIR_R(X = X, Y = Y, eps.inv = 1e-7)
+
+test <- cumuSAVE_R(X = X, Y = Y, eps.inv = 1e-7)
+
+
